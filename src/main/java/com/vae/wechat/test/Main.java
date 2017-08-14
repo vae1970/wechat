d30615d7e6eab853e10fab86853855dece380840
vae1970 <496075305@qq.com>
2017-08-14 19:08:44 +0800

    1.多线程

---
+++ src/main/java/com/vae/wechat/test/Main.java
@@ -0,0 +1,63 @@
+package com.vae.wechat.test;
+
+class Sequence {
+    private int num=1;
+    public  int getNext() {
+        return num++;
+    }
+}
+
+class SequenceThread extends Thread {
+    private Sequence sequence;
+    private int order;
+    private SequenceThread next;
+    public boolean started=false;
+    public SequenceThread(String name,int or,Sequence se){
+        this.setName(name);
+        this.sequence=se;
+        this.order=or;
+    }
+    public void setNext(SequenceThread next){
+        this.next=next;
+    }
+    public void run(){
+        int num=sequence.getNext();
+        this.started=true;
+        while(num<101){
+            if(num%4==order){
+                System.out.println(getName()+":"+num);
+                if(this.next.started){
+                    synchronized (this.next) {
+                        this.next.notify();
+                    }}else{
+                    this.next.start();
+                }
+                try {
+                    synchronized (this) {
+                        this.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                num=sequence.getNext();
+            }
+            //
+        }
+        System.exit(0);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Sequence sequ=new Sequence();
+        SequenceThread a=new SequenceThread("A",1,sequ);
+        SequenceThread b=new SequenceThread("B",2,sequ);
+        SequenceThread c=new SequenceThread("C",3,sequ);
+        SequenceThread d=new SequenceThread("D",0,sequ);
+        a.setNext(b);
+        b.setNext(c);
+        c.setNext(d);
+        d.setNext(a);
+        a.start();
+    }
+}
